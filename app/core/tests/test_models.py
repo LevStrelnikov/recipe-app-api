8adab5efcf5cc39540e4eaddf0f891320239b24e
Lev Strelnikov <levkaa@gmail.com>
2019-07-24 09:06:01 +0300

    Added super user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -14,3 +14,35 @@ class ModelTests(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email for new user is normalized"""
+
+        email = "test@LONDONAPPDEV.COM"
+        password = "TestPass123"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+        email_name, domain_part = email.strip().rsplit('@', 1)
+        normalized_email = email_name + '@' + domain_part.lower()
+        self.assertEqual(user.email, normalized_email)
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(
+                email=None,
+                password="Test123"
+            )
+
+    def test_create_new_super_user(self):
+        """Test creating super user"""
+        user = get_user_model().objects.create_superuser(
+            "test@londonappdev.com",
+            "test123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
